3e447dfde36778625944c74c4d041c0a834b4ac0
Pete Wildsmith <pwildsmith@gumtree.com>
2017-12-03 19:16:37 +0000

    don't blat the global namespace

--- gcalendar.py
+++ gcalendar.py
@@ -1,4 +1,5 @@
 import requests
+import caldav
 
 PRIVATE_ICS_URL = "https://calendar.google.com/calendar/ical/weargoggles.co.uk_ua13rcg16tm2t6c1m3jnpl6h28%40group.calendar.google.com/private-6970f1ae231269334b0d1f81b8f5c9d0/basic.ics"
 CALENDAR_ID = "weargoggles.co.uk_ua13rcg16tm2t6c1m3jnpl6h28@group.calendar.google.com"
