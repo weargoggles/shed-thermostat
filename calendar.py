21fc0a9e88e5d5856e7ac40bf93b47c8f75c3da8
Pete Wildsmith <pete@weargoggles.co.uk>
2017-12-03 18:53:47 +0000

    calendar beginnings

---
+++ calendar.py
@@ -0,0 +1,11 @@
+import requests
+
+PRIVATE_ICS_URL = "https://calendar.google.com/calendar/ical/weargoggles.co.uk_ua13rcg16tm2t6c1m3jnpl6h28%40group.calendar.google.com/private-6970f1ae231269334b0d1f81b8f5c9d0/basic.ics"
+CALENDAR_ID = "weargoggles.co.uk_ua13rcg16tm2t6c1m3jnpl6h28@group.calendar.google.com"
+
+# https://developers.google.com/google-apps/calendar/v3/reference/events/list
+
+# https://developers.google.com/google-apps/calendar/auth
+
+def events(date):
+    pass
